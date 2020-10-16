24d2632e17731534e286d7c49c4c92df8c6f9f8d
nalutz <72173600+nalutz@users.noreply.github.com>
2020-10-16 00:45:10 -0500

    Add files via upload

---
+++ eats/single.php
@@ -0,0 +1,19 @@
+<?php
+//Page Template
+
+get_header(); ?>
+
+<?php
+//start loop
+while ( have_posts() ) :
+				the_post(); ?>
+<main class="content">
+	<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+		<h2><?php the_title(); ?></h2>
+		<?php the_post_thumbnail(); ?>
+		<?php the_content(); ?>
+	</article>
+</main>
+<?php endwhile; //end loop ?>
+
+<?php get_footer(); ?>
